79503df94a1532b4ea916d88bf518657fe7b6ecf
Sheheryar Parvaz <me@cherryman.org>
2024-09-20 22:29:21 -0400

    start writing scanner

--- src/main.zig
+++ src/main.zig
@@ -17,6 +17,307 @@ pub fn main() !void {
     try bw.flush(); // don't forget to flush!
 }
 
+/// Modelled after [`std.json.Scanner`] as it's idiomatic and is
+/// approximately what I was going for. Here I cut and inlined some
+/// stuff out since the syntax is somewhat simpler.
+const Scanner = struct {
+    const Self = @This();
+
+    input: []const u8 = "",
+    cursor: usize = 0,
+    state: State = .none,
+    is_end_of_input: bool = false,
+
+    // TODO: taken from `Diagnostics`, need to use
+    line_number: usize = 1,
+    // line_start_cursor: usize = @as(usize, @bitCast(@as(isize, -1))),
+    line_start_cursor: usize = 0,
+    // total_bytes_before_current_input: usize = 0,
+    // cursor_pointer: *const usize = undefined,
+
+    // symbols: std.StringHashMap([]const u8),
+    in_comment: bool = false,
+
+    // TODO: cite https://github.com/aclements/biblib
+
+    fn init() Self {
+        return Self{};
+    }
+
+    fn deinit(self: *Self) void {
+        self.* = undefined;
+    }
+
+    const State = enum {
+        none,
+        saw_at,
+        entry_type,
+        entry_post_type,
+        // TODO no fuckin idea how comment works
+        // comment,
+        // string_set,
+        // entry_members,
+        pre_key,
+        key,
+        post_key,
+        pre_value,
+        value_quoted,
+        value_braced,
+        post_member,
+    };
+
+    const Error = error{ SyntaxError, UnexpectedEndOfInput, UnexpectedChar };
+    const NextError = Error || error{BufferUnderrun};
+
+    const Token = union(enum) {
+        part_type: []const u8,
+        part_key: []const u8,
+        end_key_no_value,
+        end_key_with_value,
+        part_value: []const u8,
+        end_value,
+        end,
+    };
+
+    /// Feeds the scanner with more input.
+    ///
+    /// Should only be called after `init` or when `next` returns
+    /// `error.BufferUnderrun`. Should _not_ be called after `finish`.
+    pub fn feed(self: *Self, input: []const u8) void {
+        assert(!self.is_end_of_input);
+        self.input = input;
+    }
+
+    /// Tells the scanner that no more input will be fed.
+    ///
+    /// Should be called exactly once. Can be called exactly after
+    /// [`feed`] or after [`next`] has returned `error.BufferUnderrun`,
+    /// as long as no more input is fed.
+    pub fn end_input(self: *Self) void {
+        assert(!self.is_end_of_input);
+        self.is_end_of_input = true;
+    }
+
+    // TODO: utf8 validation
+    // TODO: utf8 bom
+    // TODO: all possible chars in idents
+    // TODO: duplicate keys
+    // TODO: lowercase keys
+    // TODO: early input handling
+    // TODO: max lengths for idents
+    // TODO: parentheses
+
+    pub fn next(self: *Self) NextError!Token {
+        while (true) {
+            switch (self.state) {
+                .none => {
+                    switch (try self.skip_to_maybe_byte() orelse return .end) {
+                        '@' => {
+                            self.cursor += 1;
+                            self.state = .saw_at;
+                            continue;
+                        },
+                        else => return error.UnexpectedChar,
+                    }
+                },
+
+                .saw_at => {
+                    if (try self.skip_to_byte() == '{') {
+                        return error.UnexpectedChar;
+                    }
+                    self.state = .entry_type;
+                    continue;
+                },
+
+                // TODO: will need to handle comment or string set.
+                // one approach: have a static array that can store
+                // max(len("comment"), len("string")), dispatch on that,
+                // don't return partial until we're sure it's neither.
+
+                .entry_type => {
+                    const i = self.cursor;
+                    while (self.cursor < self.input.len) : (self.cursor += 1) {
+                        switch (self.input[self.cursor]) {
+                            '0'...'9', 'a'...'z', 'A'...'Z', '_' => {
+                                continue;
+                            },
+                            '{', ' ', '\n', '\t', '\r', '%' => {
+                                self.state = .entry_post_type;
+                                break;
+                            },
+                            else => return error.UnexpectedChar,
+                        }
+                    }
+                    if (i == self.cursor) {
+                        return error.BufferUnderrun;
+                    }
+                    return Token{ .part_type = self.input[i..self.cursor] };
+                },
+
+                .entry_post_type => {
+                    switch (try self.skip_to_byte()) {
+                        '{' => {
+                            self.cursor += 1;
+                            self.state = .pre_key;
+                            continue;
+                        },
+                        else => return error.UnexpectedChar,
+                    }
+                },
+
+                .pre_key => {
+                    switch (try self.skip_to_byte()) {
+                        '}' => {
+                            self.cursor += 1;
+                            self.state = .none;
+                            @panic("TODO");
+                        },
+                        '0'...'9', 'a'...'z', 'A'...'Z', '_' => {
+                            self.state = .key;
+                            continue;
+                        },
+                        else => {
+                            return error.UnexpectedChar;
+                        },
+                    }
+                },
+
+                .key => {
+                    // case should only be hit if we're at the start of a key.
+                    const i = self.cursor;
+                    while (self.cursor < self.input.len) : (self.cursor += 1) {
+                        switch (self.input[self.cursor]) {
+                            '0'...'9', 'a'...'z', 'A'...'Z', '_' => {
+                                continue;
+                            },
+                            else => {
+                                self.state = .post_key;
+                                break;
+                            },
+                        }
+                    }
+                    if (i == self.cursor) {
+                        return error.BufferUnderrun;
+                    }
+                    return Token{ .part_key = self.input[i..self.cursor] };
+                },
+
+                .post_key => {
+                    switch (try self.skip_to_byte()) {
+                        ',' => {
+                            self.cursor += 1;
+                            self.state = .pre_key;
+                            @panic("TODO");
+                        },
+                        '}' => {
+                            self.cursor += 1;
+                            self.state = .none;
+                            @panic("TODO");
+                        },
+                        '=' => {
+                            self.cursor += 1;
+                            self.state = .pre_value;
+                            continue;
+                        },
+                        ' ', '\n', '\t', '\r', '%' => {
+                            self.state = .pre_value;
+                            continue;
+                        },
+                        else => return error.UnexpectedChar,
+                    }
+                },
+
+                .pre_value => {
+                    switch (try self.skip_to_byte()) {
+                        '"' => {
+                            self.cursor += 1;
+                            self.state = .value_quoted;
+                            continue;
+                        },
+                        '{' => {
+                            self.cursor += 1;
+                            self.state = .value_braced;
+                            continue;
+                        },
+                        else => {
+                            // TODO: handle variables
+                            return error.UnexpectedChar;
+                        },
+                    }
+                },
+
+                .value_quoted => {
+                    @panic("TODO");
+                },
+
+                .value_braced => {
+                    @panic("TODO");
+                },
+
+                else => @panic("TODO"),
+            }
+        }
+    }
+
+    /// Skips whitespaces and comments until the next byte to parse.
+    /// If `is_end_of_input` is true, returns `error.UnexpectedEndOfInput`.
+    fn skip_to_byte(self: *Self) NextError!u8 {
+        return try self.skip_to_maybe_byte() orelse return error.UnexpectedEndOfInput;
+    }
+
+    /// Skips whitespaces and comments, returning `null` if `is_end_of_input`.
+    fn skip_to_maybe_byte(self: *Self) !?u8 {
+        self.skip_whitespace();
+        if (self.cursor < self.input.len) {
+            return self.input[self.cursor];
+        }
+        if (self.is_end_of_input) {
+            return null;
+        }
+        return error.BufferUnderrun;
+    }
+
+    /// Skips whitespaces and comments.
+    fn skip_whitespace(self: *Self) void {
+        while (self.cursor < self.input.len) : (self.cursor += 1) {
+            if (self.in_comment) {
+                self.skip_comment();
+                continue;
+            }
+            switch (self.input[self.cursor]) {
+                '\n' => {
+                    self.line_number += 1;
+                    self.line_start_cursor = self.cursor;
+                    continue;
+                },
+                ' ', '\t', '\r' => continue,
+                '%' => {
+                    self.in_comment = true;
+                    continue;
+                },
+                else => return,
+            }
+        }
+    }
+
+    /// If `in_comment` is true, skips until the end of the line.
+    /// Panics if `in_comment` is false.
+    fn skip_comment(self: *Self) void {
+        assert(self.in_comment);
+        while (self.cursor < self.input.len) : (self.cursor += 1) {
+            switch (self.input[self.cursor]) {
+                '\n' => {
+                    self.line_number += 1;
+                    self.line_start_cursor = self.cursor;
+                    self.in_comment = false;
+                    continue;
+                },
+                else => continue,
+            }
+        }
+    }
+};
+
 fn skipws(str: *[]const u8) void {
     for (str.*, 0..) |c, i| {
         if (!ascii.isWhitespace(c)) {
@@ -26,18 +327,6 @@ fn skipws(str: *[]const u8) void {
     }
     str.* = str.*[str.*.len..];
 }
-
-// fn parsetok(str: []const u8) struct { tok: ?[]const u8, rst: []const u8 } {
-//     for (str, 0..) |c, i| {
-//         if (!ascii.isAlphabetic(c)) {
-//             return .{ .tok = str[0..i], .rst = str[i..] };
-//         }
-//
-//         // TODO handle not ascii
-//         // TODO handle all other shit
-//     }
-//     return .{ .tok = null, .rst = str[str.len..] };
-// }
 
 /// Parses an ascii encoded word.
 fn parse_word(str: *[]const u8) ![]const u8 {
@@ -160,9 +449,6 @@ fn parse_entry(alloc: std.mem.Allocator, str: *[]const u8) !Entry {
         const tag_value = try parse_tag_value(str);
         try entry.push(tag_value.tag, tag_value.value);
 
-        // TODO: duplicate keys
-        // TODO: lowercase keys
-
         skipws(str);
 
         if (str.*.len == 0) {
@@ -191,7 +477,6 @@ fn parse(alloc: std.mem.Allocator, s: []const u8) !std.ArrayList(Entry) {
 
     skipws(&str);
     while (str.len > 0) {
-        // TODO: comments
         const entry = try parse_entry(alloc, &str);
         entries.append(entry) catch return error.Alloc;
         skipws(&str);
@@ -310,4 +595,14 @@ test "it fucking works" {
     assert(eql(u8, entry.elems.items[3].value.?, "10"));
     assert(eql(u8, entry.elems.items[4].key, "publisher"));
     assert(eql(u8, entry.elems.items[4].value.?, "American Chemical Society"));
+
+    var scan = Scanner.init();
+    scan.feed(test_str);
+    scan.end_input();
+    while (true) {
+        switch (try scan.next()) {
+            .end => break,
+            else => |c| std.debug.print("{any}\n", .{c}),
+        }
+    }
 }
