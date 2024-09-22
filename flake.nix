9337a5e85ba82ae4a4313f80ba9b03623c08357f
Sheheryar Parvaz <me@cherryman.org>
2024-09-21 20:00:14 -0400

    add zig overlay for 0.14.0 nightly

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "bibtools";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    zig-overlay = {
+      url = "github:mitchellh/zig-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports =
+        [
+        ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          packages.default = pkgs.hello;
+
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              inputs.zig-overlay.packages.${system}."master-2024-09-20"
+            ];
+          };
+        };
+      flake =
+        {
+        };
+    };
+}
